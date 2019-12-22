a95f68eceff576c158dd157c8a4ac164898cfe2a
“Manny <mgonzalez@starry.com>
2019-12-21 21:00:15 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/layout/Navbar.jsx'
+import Jumbotron from './components/layout/Jumbotron'
+import './styles/styles.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Navbar />
+    <Jumbotron />
+  </div>
 
+
+)
 export default App;
---
+++ src/components/layout/Jumbotron.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+export class Jumbotron extends Component {
+  render() {
+    return (
+      <div className='jumbotron container'>
+        <h1 className='display-4'>Hello, React!</h1>
+        <p className='lead'>
+          This is a quick one page App crated while learning React.
+        </p>
+        <a
+          className='btn btn-primary btn-lg'
+          href='https://reactjs.org/'
+          target='_blank'
+          role='button'
+          rel="noopener noreferrer"
+        >
+          Learn more
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Jumbotron;
