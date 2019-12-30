16d1d6dd47886bc474a792ec348551e686e7e132
“Manny <mgonzalez@starry.com>
2019-12-29 23:29:32 -0500

    Test Commit

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const NotFound = () => (
   <div id='err-page'>
-  <h1>Error 404</h1>
-  <p>Page Not Found !</p>
-</div>
+    <h1>Error 404</h1>
+    <p>Page Not Found </p>
+  </div>
 );
 
 export default NotFound;
